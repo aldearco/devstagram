731240d1d298a1e07d007c791ce2442d4b5e8652
aldearco <albertodelarco@outlook.com>
2022-05-04 21:42:35 +0200

    Controlador de Subida y procesado de imagen

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request){
         $imagen = $request->file('file');
 
-        return response()->json(['imagen' => $imagen->extension() ]);
+        $nombreImagen = Str::uuid().'.'.$imagen->extension();
+        
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1080, 1080);
+
+        $fecha = Carbon::now();
+        $imagenPath = public_path('uploads').'/'.$nombreImagen;
+
+        $imagenServidor->save($imagenPath);
+
+
+        return response()->json(['imagen' => $nombreImagen ]);
     }
 }
