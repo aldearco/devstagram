898b26a14d484003d309f9e63855f01bde1a08b5
aldearco <albertodelarco@outlook.com>
2022-05-17 17:24:14 +0200

    Componentes reutilitzables de Laravel (Mostrar Posts)

---
+++ resources/views/components/listar-post.blade.php
@@ -0,0 +1,19 @@
+<div>
+    @if($posts->count())
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            @foreach($posts as $post)
+                <div>
+                    <a href="{{ route('posts.show', ['post'=>$post, 'user' => $post->user ]) }}">
+                        <img src="{{ asset('uploads').'/'.$post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="my-10">
+            {{ $posts->links() }}
+        </div>
+    @else
+        <p class="text-center text-gray-700 font-bold">Aún no hay posts.</p>
+    @endif
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,24 +61,6 @@ Perfil de {{ $user->username }}
 
 <section class="container mx-auto mt-10">
     <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-    @if($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['post'=>$post, 'user' => $post->user ]) }}">
-                        <img src="{{ asset('uploads').'/'.$post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="my-10">
-            {{ $posts->links() }}
-        </div>
-    @else
-        <p class="text-gray-600 uppercase text-sm text-center font-bold">Aún no hay posts.</p>
-    @endif
-
-
+    <x-listar-post :posts="$posts" />
 </section>
 @endsection
